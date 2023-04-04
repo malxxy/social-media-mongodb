241430116d232cb1577438ecee5ff5fd689b1a8d
malxxy <maliayraguen@gmail.com>
2023-04-04 10:36:06 -0700

    User model complete except validator for email

--- models/User.js
+++ models/User.js
@@ -1,41 +1,31 @@
-// username
-// string
-// unique
-// required
-// trimmed
-
 // email
-// required
-// unique
-// must match a vvalid email adress
-
-// thoughts
-// array of id values referencing thoughts model
-
-// friends
-// array of id values referencng user model (self reference)
-
-// virutal called friendCount that retrieves the length of the users firends array
+// must match a valid email adress
 
 const { Schema, model } = require('mongoose');
+const Thought = require('./Thought');
 
 // Schema to create User model
 const userSchema = new Schema(
   {
-    username: String,
-    email: String,
-    thoughts: Array,
-    friends: Array,
+    username: {type: String, required: true,  unique: true,  trim: true },
+    email: {type: String, required: true,  unique: true},
+    thoughts: [Thought],
+    friends: [User],
   },
   {
-    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
     toJSON: {
       virtuals: true,
     },
     id: false,
   }
-);
+  );
+  
+ // virutal called friendCount that retrieves the length of the users friends array
+userSchema  
+  .virtual('getFriends')
+.get(function () {
+  return this.friends.length;
+});
 
 // Initialize our User model
 const User = model('user', userSchema);
