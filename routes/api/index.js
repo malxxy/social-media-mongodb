c21aab606dcad1e0307df535513007995171cafa
malxxy <maliayraguen@gmail.com>
2023-04-03 18:16:15 -0700

    api index routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,10 @@
+const router = require('express').Router();
 const users = require('./users');
 const thoughts = require('./thoughts');
 const reactions = require('./reactions');
 
+router.use('/users', users);
+router.use('/thoughts', thoughts);
+router.use('/reactions', reactions);
+
+module.exports = router;
