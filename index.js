9956c604908fc3b81e23226d3c06053a6dc35811
malxxy <maliayraguen@gmail.com>
2023-04-04 13:25:52 -0700

    package.json and index.js fix

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const db = require('./config/connection')
-
-const { Reaction, Thought, User} = require('./models');
+const routes = require('./routes')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-
-app.get('INSERT CORRET', (req,res) => {
-    Item.find((err,result) => {
-        if (err) {
-            res.status(500).send({ message: 'Internal Server Error'});
-        } else {
-            res.status(200).json(result);
-        }
-    });
-});
+app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
