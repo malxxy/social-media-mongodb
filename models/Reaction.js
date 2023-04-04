8c593b3b1982a94682d54bbeca62a4f899466aff
malxxy <maliayraguen@gmail.com>
2023-04-03 18:37:22 -0700

    reaction model schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const Tag = require('./Tag');
 
 // reactionID
 // use mongoose objectId data type
--- models/index.js
+++ models/index.js
@@ -2,4 +2,4 @@ const Reaction = require('./Reaction');
 const Thought = require('./Thought');
 const User = require('./User');
 
-module.exports = { Reaction, Thought, User}
+module.exports = { Reaction, Thought, User };
