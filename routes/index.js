520fbd01369e5910b9a1d4c8b24a7adf9422245c
malxxy <maliayraguen@gmail.com>
2023-04-03 18:19:58 -0700

    add api path to all routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,10 @@
-const Users = require('./users');
-const Thoughts = require('./Thoughts');
+const router = require('express').Router();
+const apiRoutes = require('./api'); // require api folder 
 
-module.exports = { Users, Thoughts };
+router.use('./api',apiRoutes); // add api path to all routes in api folder
+
+router.use((req,res) => {
+    return res.send('Incorrect route'); // send incorrect route message if accessed route does not exist
+});
+
+module.exports = router;
