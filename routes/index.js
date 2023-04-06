657c2f28ba2273446b29dd249fb8f792b17c4ad9
malxxy <maliayraguen@gmail.com>
2023-04-06 15:00:42 -0700

    condense index route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ const apiRoutes = require('./api'); // require api folder
 
 router.use('./api',apiRoutes); // add api path to all routes in api folder
 
-router.use((req,res) => {
-    return res.send('Incorrect route'); // send incorrect route message if accessed route does not exist
-});
+router.use((req,res) => res.send('Incorrect route')); // send incorrect route message if accessed route does not exist
 
 module.exports = router;
