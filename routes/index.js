80f0a550d4f6128d4c870c4ca80293dbf5babf8e
malxxy <maliayraguen@gmail.com>
2023-04-06 19:22:16 -0700

    DEBUG ROUTE ERRORS

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api'); // require api folder 
 
-router.use('./api',apiRoutes); // add api path to all routes in api folder
+router.use('/api',apiRoutes); // add api path to all routes in api folder
 
 router.use((req,res) => res.send('Incorrect route')); // send incorrect route message if accessed route does not exist
 
